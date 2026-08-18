bdc3bbba3a5592c471052c5a0ae5a5c28906707c
LiquidPlummer <kyle.plummer@revature.com>
2026-08-18 10:23:28 -0400

    logging example

---
+++ Demos/Spring/logging/src/main/java/com/revature/spring_starter/components/LoggingExample.java
@@ -0,0 +1,9 @@
+package com.revature.spring_starter.components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoggingExample {
+    
+
+}
---
+++ Demos/Spring/logging/src/main/java/com/revature/spring_starter/repositories/MyRepository.java
@@ -0,0 +1,7 @@
+package com.revature.spring_starter.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MyRepository {
+}
